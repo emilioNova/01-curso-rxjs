76d0ed59da0ffcd30688f470254382cf8f50dd9f
emilioNova <37042917+emilioNova@users.noreply.github.com>
2022-06-20 10:25:07 +0200

    Hola mundo observable

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
+import { Observable } from 'rxjs'
+
+console.log('Hola Mundo!');
+
+const nombre = 'Emilio';
+
+// Forma antigua deprecada!!!
+//const obs$ = Observable.create();
 
 
-console.log('Hola Mundoffffffffffffffffffff!');
+const obs$ = new Observable<string>( sub => {
+
+    sub.next('hola');
+    sub.next(' ');
+    sub.next(nombre);
+    sub.next(' HHIIIIIIII eeee ');
+    sub.complete();
 
 
+    sub.next('DISPUES DEL COMPLETE');
+
+} );
+
+
+obs$.subscribe( console.log);
 
 
 
